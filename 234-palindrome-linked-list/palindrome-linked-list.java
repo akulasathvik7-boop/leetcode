0cff49c1d4fbc7824cf7120f370cb0692b934b82
sathvik <akulasathvik7@gmail.com>
2026-08-07 16:32:22 +0530

    Time: 6 ms (35.41%) | Memory: 94.2 MB (80.33%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -9,22 +9,42 @@
  * }
  */
 class Solution {
+
+    public static ListNode find_middle(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+
+    }
+    public static ListNode reverse(ListNode head){
+        ListNode curr=head;
+        ListNode prev=null;
+        while(curr!=null){
+            ListNode nextNode=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=nextNode;
+        }
+        // head.next=null;
+        head=prev;
+        return head;
+    }
     public boolean isPalindrome(ListNode head) {
-        Stack<Integer> stack=new Stack<>();
-        ListNode temp=head;
-        while(temp!=null){
-            stack.add(temp.val);
-            temp=temp.next;
-        }
-        ListNode temp1=head;
-        while(temp1!=null){
-            if(temp1.val==stack.peek()){
-                  temp1=temp1.next;
-                stack.pop();
-            }else{
-                return false;
-                            }
-        }
-        return true;
+       if(head ==null&&head.next==null)return true;
+       ListNode start1=head;
+       ListNode middle=find_middle(head);
+       ListNode start2=reverse(middle.next);
+
+       while(start2!=null){
+        if(start1.val!=start2.val)return false;
+
+        start1=start1.next;
+        start2=start2.next;
+       }
+       return true;
     }
 }
